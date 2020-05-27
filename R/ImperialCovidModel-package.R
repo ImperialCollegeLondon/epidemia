e20c0f6bee6e85c6a4379831abc41929bab68f14
James Scott <james.scott15@imperial.ac.uk>
2020-05-27 16:26:42 +0200

    Package structure from rstantools::rstan_create_package()

---
+++ R/ImperialCovidModel-package.R
@@ -0,0 +1,16 @@
+#' The 'ImperialCovidModel' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name ImperialCovidModel-package
+#' @aliases ImperialCovidModel
+#' @useDynLib ImperialCovidModel, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
