62706236818a6ce864f09485ca593e6afca15a28
Ubuntu <ubuntu@ip-172-31-3-72.eu-west-2.compute.internal>
2020-06-03 08:10:43 +0000

    added a "big test" for epim

--- tests/testthat/test-basic-no_covar.R
+++ tests/testthat/test-basic-no_covar.R
@@ -25,4 +25,32 @@ test_that("constant R per region",{
 
     ## generate stan file
     expect_error(rstan::sampling(object=stanmodels$base, data=standata, chains=0), NA) ## not expecting an error here
+
 })
+
+
+test_that("constant R per region - epim",{
+    data(USA)
+    # two groups
+    for (name in names(USA)) 
+      assign(name, USA[[name]])
+
+   w<-is.element(data$code, c("NY","WA"))
+    data<-data[w,]
+    
+
+                                        # formula
+    formula <- Rt(code, date) ~ 0 
+
+    expect_error(fit <- epim(formula, 
+                        data=data, 
+                            obs=obs, 
+                            pops=pops, 
+                            ifr=ifr, 
+                            si=si, 
+                        seed_days = 6, chains=0)
+                 )
+
+})
+
+
