8e10be20bf5e4a5d5e5c8d09fda5b0c80b8f496b
James Scott <james.scott15@imperial.ac.uk>
2020-06-05 11:17:50 +0200

    Updated documentation of epim

--- R/epim.R
+++ R/epim.R
@@ -2,13 +2,11 @@
 #' 
 #' @param formula An R object of class `formula`. The left hand side must take the form `Rt(group,date)', with 'group' representing a factor vector indicating group membership (i.e. country, state, age cohort), and 'code' being a vector of Date objects.
 #' @param data A dataframe with columns corresponding to the terms appearing in 'formula'. See [lm].
-#' @param obs A named list giving available observations
-#' * deaths: A three column dataframe representing death data. The first column represents group membership and must be coercible to class 'factor'. The second column indicates the observation date and must be coercible to class `Date'.
-#' * incidence: Same as 'deaths', although giving incidence data.
-#' * dtd: A vector representing 'days to observation'. Like si, this is a probability vector. The nth element giving the probability that the observation event (incidence/death) occurs n days after an individual is infected.
-#' * dti: same as 'dtd', but representing time until an incidence is recorded after onset of infection.
+#' @param obs A list of lists giving available observations. Each element of 'obs' must be a list containing three elements
+#' * First element: A three column dataframe representing some type of observed data. Examples include recorded incidence, deaths or hospitalisations. The first column represents group membership and must be coercible to class 'factor'. The second column indicates the observation date and must be coercible to class `Date'. The third column contain the actual data.
+#' * Second element: A two column dataframe giving the estimated proportion of infected individuals recorded as an observation. For example, if recording deaths, this would be some estimate of each groups infection fatality ratio (IFR). First column represents the group, while the second the corresponding proportion.
+#' * Third Element: A probability vector. Conditional on an observation 'event' (i.e. a single death or hospitalisation etc.), the nth element represents the probability that the individual was infected n days prior to this.
 #' @param pops  A two column dataframe giving the total population of each group. First column represents the group, while the second the corresponding population.
-#' @param ifr A two column dataframe giving the infection fatality rate in each group. First column represents the group, while the second the corresponding IFR.
 #' @param si A vector representing the serial interval of the disease (a probability vector).
 #' @param seed_days Number of days for which to seed infections.
 #' @param ... Arguments allowed in rstanarm::stan_glmer(). For example one can control the prior distribution of the covariates.
@@ -19,7 +17,6 @@ epim <-
            data,
            obs,
            pops,
-           ifr,
            si,
            seed_days = 6,
            algorithm = c("sampling", "meanfield", "fullrank"),
@@ -33,7 +30,6 @@ epim <-
   groups  <- levels(data$group)
   obs     <- checkObs(data, obs)
   pops    <- checkPops(pops, groups)
-  ifr     <- checkIFR(ifr, groups)
   si      <- checkSV(si)
 
   if (seed_days < 1)
