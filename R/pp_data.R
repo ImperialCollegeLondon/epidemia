b6042eccb6e84915287b593b65245098038117c7
jscott6 <james.scott15@imperial.ac.uk>
2020-06-28 12:54:58 +0200

    first attempt at constructing model matrix

---
+++ R/pp_data.R
@@ -0,0 +1,26 @@
+
+#' Given new data, generates the fixed effects model matrix
+#' 
+#' @export
+fe_mat <- function(object, newdata) {
+  
+  checkData(formula(object), newdata, NULL)
+  groups <- levels(newdata$group)
+  w <- !(groups %in% object$groups)
+  if (any(w))
+    stop(paste0("Groups ", groups[w], " not modeled. 'newdata' only supported for existing populations."))
+  
+  form <- formula(object, fixed.only=TRUE)
+  trms <- delete.response(terms(object, fixed.only=TRUE)
+  mf <- model.frame(object)
+  mf <- mf[all.vars(trms)]
+  
+  isFac <- vapply(mf, is.factor, FUN.VALUE = TRUE)
+  orig_levs <- if (length(isFac) == 0) 
+    NULL else lapply(mf[isFac], levels)
+  
+  mfnew <- model.frame(trms, newdata, xlev = orig_levs)
+  x <- model.matrix(formula(trms), data = mfnew)
+  return(x)
+}
+
