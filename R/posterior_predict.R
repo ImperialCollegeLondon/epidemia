1e45f85ca9feba450eb625ab176c23e65d467143
jscott6 <james.scott15@imperial.ac.uk>
2020-06-29 14:05:04 +0200

    Written parse_obs

--- R/posterior_predict.R
+++ R/posterior_predict.R
@@ -1,5 +1,40 @@
 
 
+
+posterior_sims <- function(object, newdata, draws=NULL, seed=NULL, ...) {
+  if (!is.null(seed))
+    set.seed(seed)
+  dots <- list(...)
+  
+  # validate the new data
+  newdata <- checkData(formula(object), newdata, NULL)
+  groups <- levels(newdata$group)
+  w <- !(groups %in% object$groups)
+  if (any(w))
+    stop(paste0("Groups ", groups[w], " not modeled. 'newdata' only supported for existing populations."))
+
+  stanmat <- as.matrix(object$stanfit)
+  stanmat <- subsamp(object, stanmat, draws)
+
+  # Construct linear predictor eta
+  dat <- pp_data(object=object, newdata=newdata, ...)
+  eta = pp_eta(object, dat, stanmat)
+  colnames(eta) <- paste0("eta[",1:ncol(eta),"]")
+
+  standata <- pp_standata(object, newdata)
+  stanmat <- pp_stanmat(object, stanmat, groups)
+  stanmat <- cbind(stanmat, eta)
+
+  sims <- rstan::gqs(stanmodels$pp_base, 
+                     data = standata, 
+                     draws=stanmat)
+
+}
+
+
+
+
+
 #' Given new data for modeled populations, generates outcome data based on fitted model
 #' 
 #' @export
@@ -35,22 +70,27 @@ posterior_predict.epimodel <- function(object, newdata, draws=NULL, seed=NULL, .
 
 }
 
-.parse_latent <- function(draws, data, nme) {
+
+# Parses a given latent quantity from the result of rstan::qgs
+#
+# @param draws Result of rstan::gqs in posterior_predict
+# @param data data.frame used (data from epim or newdata)
+# @param nme Name of the latent variable to return
+parse_latent <- function(sims, data, nme) {
 
   # get useful quantities
   sdat <- get_sdat_data(data)
   for (name in names(sdat))
     assign(name, sdat[[name]])
 
-  draws <- rstan::extract(draws, nmse)[[1]]
+  sims <- rstan::extract(sims, nme)[[1]]
 
   out <- list()
   for (i in seq_along(groups)) {
     t <- starts[i]:ends[i]
-    df <- as.data.frame(t(draws[,t,i]))
+    df <- as.data.frame(t(sims[,t,i]))
 
     w <- data$group %in% groups[i]
-    
     df <- do.call("cbind.data.frame", 
                   args = list(date = data$date[w], 
                               df))
@@ -61,88 +101,35 @@ posterior_predict.epimodel <- function(object, newdata, draws=NULL, seed=NULL, .
   return(out)
 }
 
+# Parses a given latent quantity from the result of rstan::qgs
+#
+# @param sims Result of rstan::gqs in posterior_predict
+# @param data data.frame used (data from epim or newdata)
+# @param nme Name of the latent variable to return
+parse_obs <- function(sims, data, obs, type) {
 
+  types <- names(obs)
+  if (!(type %in% types))
+    stop(paste0("'",type,"' is not an observation type."))
 
-.parse_latents <- function(draws, data) {
+  idx <- which(type == types)
+  sims <- rstan::extract(sims, "pred")[[1]]
 
   # get useful quantities
   sdat <- get_sdat_data(data)
   for (name in names(sdat))
     assign(name, sdat[[name]])
 
-  rt_unadj    <- rstan::extract(draws, "Rt_unadj")[[1]]
-  rt          <- rstan::extract(draws, "Rt")[[1]]
-  infections  <- rstan::extract(draws, "infections")[[1]]
-
-  rout <- list()
-  for (i in seq_along(groups)) {
-    t <- starts[i]:ends[i]
-    df <- as.data.frame(t(rt[,t,i]))
-
-    w <- data$group
-  }
-
-
-}
-
-# Parses the time varying reproduction number from the result of rstan::qgs
-#
-# @param draws The result of rstan::gqs in posterior_predict
-# @param data The data.frame used
-# @param standata 
-
-.parse_rt <- function(draws, data, standata) {
-
-  groups <- levels(object$data$group)
-
-  rt <- rstan::extract(draws, "Rt")[[1]]
-  starts <- standata$starts
-  ends <- starts + standata$NC - 1
-
   out <- list()
   for (i in seq_along(groups)) {
     t <- starts[i]:ends[i]
-    df <- as.data.frame(t(rt[,t,i]))
-
-    # attach corresponding dates
-    w <- data$group %in% groups[i]
-    df <- do.call("cbind.data.frame",
-                  args = list(date= data$date[w],
-                              df))
-    colnames(df) <- c("date", paste0("draw", 1:(ncol(df)-1)))
-    out[[groups[[i]]]] <- df
-  }
-  return(out)
-}
-
-
-.parse_obs. <- function(object, type = NULL, ...) {
-  
-  types <- names(object$obs)
-  if (!(type %in% types))
-    stop(paste0("'",type,"' is not an observation type."))
-  idx <- which(type == types)
-  
-  
-  res <- underlyings(object)
-  pred <- rstan::extract(res, "pred")[[1]]
-  
-  groups <- levels(object$data$group)
-  
-  # get indices for each group
-  starts  <- object$standata$starts
-  ends    <- starts + object$standata$NC - 1
-  out <- list()
-  
-  for (i in seq_along(groups)) {
-    t <- starts[i]:ends[i]
-    df <- t(pred[,idx,t,i])
+    df <- t(sims[,idx,t,i])
     df <- as.data.frame(df)
     
     # attach corresponding dates
-    w <- object$data$group %in% groups[i]
+    w <- data$group %in% groups[i]
     df <- do.call("cbind.data.frame", 
-                  args = list(date = object$data$date[w], 
+                  args = list(date = data$date[w], 
                               df))
     
     colnames(df) <- c("date", paste0("draw", 1:(ncol(df)-1)))
@@ -150,7 +137,6 @@ posterior_predict.epimodel <- function(object, newdata, draws=NULL, seed=NULL, .
   }
   return(out)
 }
-
 
 subsamp <- function(object, mat, draws=NULL) {
 
