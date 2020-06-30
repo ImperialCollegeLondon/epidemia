17c878f9da4bdd392868caebc38ecde56a226b3a
jscott6 <james.scott15@imperial.ac.uk>
2020-06-30 17:25:34 +0200

    Modified plot_infections

--- R/plot_Rt.R
+++ R/plot_Rt.R
@@ -1,9 +1,7 @@
 #' Plotting the time-varying reproduction rates
 #'
-#' Plots credible intervals for the time-varying reproduction number.
-#' The user can control the level and number of credible intervals and
-#' the plotted group(s).
-#' 
+#' Plots credible intervals for the time-varying reproduction rate.
+#' The user can control the levels of the intervals and the plotted group(s).
 #' This is a generic function.
 #' 
 #' @templateVar epimodelArg object
@@ -25,7 +23,6 @@ plot_rt.epimodel <- function(object, group=NULL, levels=c(50,95), log10_scale=FA
   levels <- .check_levels(levels)
   if(!is.logical(log10_scale))
     stop("log10_scale must be of type logical", call. = FALSE)
-
 
   rt <- posterior_rt(object=object, ...)
 
@@ -76,25 +73,18 @@ plot_rt.epimodel <- function(object, group=NULL, levels=c(50,95), log10_scale=FA
 
 #' Plotting the posterior predictive distribution
 #'
-#' Plots credible intervals for the observed data under the posterior predictive.
+#' Plots credible intervals for the observed data under the posterior predictive distribution.
 #' Plots for a specific observation type. 
-#' The user can control the level and number of credible intervals and the plotted group(s).
-#' 
+#' The user can control the levels of the intervals and the plotted group(s).
 #' This is a generic function.
 #' 
-#' @templateVar epimodelArg object
-#' @template args-epimodel-object 
+#' @inherit plot_rt params return
 #' @param type the name of the observations to plot. This should match one of the names
 #' of the \code{obs} argument to \code{epim}.
 #' @param posterior_mean If true, the credible intervals are plotted for the posterior mean. Defaults to FALSE, 
 #'  in which case the posterior predictive is plotted.
-#' @param group \code{NULL}, string or character vector specifying which groups
-#' to plot. Default is \code{NULL}, which plots all possible groups.
-#' @param levels numeric vector giving the levels of the plotted credible intervals
-#' Logical, default is \code{FALSE}.
 #' @param ... Additional arguments for \code{\link[epidemia]{posterior_predict}}. Examples include \code{newdata}, which allows 
 #'  predictions or counterfactuals.
-#' @return A ggplot object.
 #' @export
 plot_obs <- function(object, ...) UseMethod("plot_obs", object)
 
@@ -156,90 +146,6 @@ plot_obs.epimodel <- function(object, type=NULL, posterior_mean=FALSE, group=NUL
     ggplot2::theme(legend.position="right") + 
     ggplot2::facet_wrap(~group, scale = "free_y")
   
-  
-  return(p)
-}
-
-
-
-
-#' Plotting the posterior predictive distribution
-#'
-#' Plots credible intervals for the observed data under the posterior predictive.
-#' Plots for a specific observation type. 
-#' The user can control the level and number of credible intervals and the plotted group(s).
-#' 
-#' This is a generic function.
-#' 
-#' @templateVar epimodelArg object
-#' @template args-epimodel-object 
-#' @param type the name of the observations to plot. This should match one of the names
-#' of the \code{obs} argument to \code{epim}.
-#' @param group \code{NULL}, string or character vector specifying which groups
-#' to plot. Default is \code{NULL}, which plots all possible groups.
-#' @param levels numeric vector giving the levels of the plotted credible intervals
-#' @param log10_scale whether to plot the reproduction number on a log10-scale.
-#' Logical, default is \code{FALSE}.
-#' @param ... not yet implemented
-#' @return A ggplot object.
-#' @export
-plot_obs_old <- function(object, ...) UseMethod("plot_obs", object)
-
-#' @rdname plot_obs
-#' @export
-plot_obs_old.epimodel <- function(object, type = NULL, group = NULL, levels = c(50, 95), ...) {
-  
-  if(!(type %in% names(object$obs)))
-    stop(paste0("obs does not contain any observations for type '", type, "'"))
-  
-  # input checks
-  group <- .check_plot_groups(object, group)
-  levels <- .check_levels(levels)
-  
-  # compute draws by group
-  obs <- lapply(group, function(g) get_obs(object, type)[[g]])
-  names(obs) <- group
-  
-  # quantiles by group
-  qtl <- lapply(obs, function(.obs) .get_quantiles(.obs, levels))
-  qtl <- data.table::rbindlist(qtl, idcol="group")
-  
-  # observed data
-  df <- object$obs[[type]][["odata"]]
-  w  <- df$group %in% group
-  df <- df[w,]
-  
-  p <-  ggplot2::ggplot(qtl) + 
-    ggplot2::geom_bar(data = df, 
-                      ggplot2::aes(x = date, y = obs, fill = "reported"),
-                      fill = "coral4", 
-                      stat='identity', 
-                      alpha=0.5) + 
-    ggplot2::geom_ribbon(data = qtl, 
-                         ggplot2::aes(x = date,
-                                      ymin=low, 
-                                      ymax=up, 
-                                      fill=tag)) +
-    ggplot2::xlab("") +
-    ggplot2::ylab(type) +
-    ggplot2::scale_y_continuous(labels = scales::comma, 
-                                expand=ggplot2::expansion(mult=c(0,0.1))) +
-    ggplot2::scale_x_date(date_breaks = "2 weeks", 
-                          labels = scales::date_format("%e %b")) + 
-    ggplot2::scale_fill_manual(name = "Credible Intervals", 
-                               labels = paste0(levels,"%"), 
-                               values = ggplot2::alpha("deepskyblue4", 
-                                                       0.55 - 0.1   * (1 - (seq_along(levels)-1)/length(levels)))) + 
-    ggplot2::theme_bw() + 
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1), 
-                   legend.position = "None", 
-                   axis.text = ggplot2::element_text(size = 12),
-                   axis.title = ggplot2::element_text(size = 12)) + 
-    ggplot2::guides(fill=ggplot2::guide_legend(ncol=1)) +
-    ggplot2::theme(legend.position="right") + 
-    ggplot2::facet_wrap(~group, scale = "free_y")
-  
-  
   return(p)
 }
 
@@ -247,34 +153,30 @@ plot_obs_old.epimodel <- function(object, type = NULL, group = NULL, levels = c(
 #' Plotting the underlying number of infections over time
 #'
 #' Plots credible intervals for the underlying number of infections.
-#' The user can control the level and number of credible intervals and the plotted group(s).
-#' 
+#' The user can control the levels of the intervals and the plotted group(s).
 #' This is a generic function.
 #' 
-#' @templateVar epimodelArg object
-#' @template args-epimodel-object 
-#' @param group \code{NULL}, string or character vector specifying which groups
-#' to plot. Default is \code{NULL}, which plots all possible groups.
-#' @param levels numeric vector giving the levels of the plotted credible intervals
-#' @param log10_scale whether to plot the reproduction number on a log10-scale.
-#' Logical, default is \code{FALSE}.
-#' @param ... not yet implemented
-#' @return A ggplot object.
+#' @inherit plot_rt params return
+#' @param ... Additional arguments for \code{\link[epidemia]{posterior_infections}}. Examples include \code{newdata}, which allows 
+#'  predictions or counterfactuals.
 #' @export
 plot_infections <- function(object, ...) UseMethod("plot_infections", object)
 
 #' @rdname plot_infections
 #' @export
 plot_infections.epimodel <- function(object, group = NULL, levels = c(50, 95), ...) {
-  
-  # input checks
-  group <- .check_plot_groups(object, group)
+
   levels <- .check_levels(levels)
-  
-  # compute draws by group
-  inf <- lapply(group, function(g) get_infections(object)[[g]])
-  names(inf) <- group
-  
+
+  inf <- posterior_infections(object=object, ...)
+
+  if (!is.null(group)) {
+    w <- !(group %in% names(rt))
+    if (any(w))
+      stop(paste0("group(s) ", group[w], " not found."), call.=FALSE)
+      inf <- inf[group]
+  }
+
   # quantiles by group
   qtl <- lapply(inf, function(.inf) .get_quantiles(.inf, levels))
   qtl <- data.table::rbindlist(qtl, idcol="group")
@@ -329,21 +231,6 @@ plot_infections.epimodel <- function(object, group = NULL, levels = c(50, 95), .
   f <- function(x) quantile(x, 0.5 + levels/200)
   qtl$up <- matrix(t(apply(df, 2, FUN=f)), ncol=1, byrow = F)
   return(qtl)
-}
-
-# Internal
-# checks that all elements of group were modelled in object
-# if group=NULL returns all the groups found in object
-.check_plot_groups <- function(object, group) {
-  if(!is.null(group)) {
-      modelled_groups <- levels(object$data$group)
-      missing_groups <- !(group %in% modelled_groups)
-    if (any(missing_groups)) {
-      missing_groups <- group[missing_groups]
-      stop(paste0("Group(s) '", paste0(missing_groups, collapse=", "), "' are not modelled."), call. = FALSE)
-    }
-  } else group <- levels(object$data$group)
-  return(group)
 }
 
 # Internal
