62a3edb2647e3c635dec0e46b4eb04843395db0a
jscott6 <james.scott15@imperial.ac.uk>
2020-07-12 15:32:55 +0100

    R CMD Check note for NSE gone

--- R/epidemia-package.R
+++ R/epidemia-package.R
@@ -23,6 +23,7 @@
 #' @importFrom rstan vb
 #' @importFrom Rdpack reprompt
 #' @importFrom utils tail
+#' @importFrom magrittr %>%
 #' @references
 #' \insertAllCited()
 #' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
