573ce84a0bc610484b875760af0ead57cc452d4c
jscott6 <james.scott15@imperial.ac.uk>
2020-07-22 19:15:48 +0100

    Added series of checks for epiobs constructor

--- R/epiobs.R
+++ R/epiobs.R
@@ -29,6 +29,10 @@ epiobs <- function(formula, data, prior, prior_intercept, offset, pvec) {
 }
 
 
+# Check 'formula' passed to epiobs meets requirements for constructing 
+# the object
+#
+# @param formula
 check_obs_formula <- function(formula) {
   if (!inherits(formula, "formula"))
     stop("'formula' must have class formula.", call. = FALSE)
@@ -39,4 +43,72 @@ check_obs_formula <- function(formula) {
   if(!match)
     stop ("left hand side 'formula' does not have required form.")
   return(formula)
+}
+
+
+# Performs a series of checks on the 'data' argument passed to epiobs
+# constructor.
+#
+# @param formula
+# @param data
+check_obs_data <- function(formula, data) {
+  stopifnot(is.data.frame(data))
+
+  vars <- all.vars(formula)
+  vars <- c(vars, get_obs(formula))
+  not_in_df <- !(vars %in% colnames(data))
+  if (any(not_in_df))
+    stop(paste(c("Could not find column(s) ", vars[not_in_df], " in 'data'"), collapse=" "), call.=FALSE)
+
+  data <- data[,vars]  # remove redundant columns
+  group <- get_group(formula)
+  time <- get_time(formula)
+
+  data <- tryCatch(
+    {
+      data[,group] <- droplevels(as.factor(data[,group))
+      data[,time] <- as.Date(data[,time])
+      data
+    },
+    error = function(cond) {
+      stop(paste0("Columns ", group, " and ", time, " are not coercible to Factor and Date Respectively. Original message: ", cond))
+    }
+  )
+
+  if(anyNA(data[,group]))
+    stop(paste0("NAs exist in data$", group, " after coercion to factor"), call. = FALSE)
+  if(anyNA(data[,date]))
+    stop(paste0("NAs exist in data$", time, " after coercion to Date"), call. = FALSE)
+
+  return(data)
+}
+
+# Get name of observation column from formula
+# @param x A formula
+get_obs <- function(x) {
+  out <- deparse(lhs(x))
+  out <- sub("\\(.*","", out)
+  return (out)
+}
+
+# Get name of group column from formula
+# @param x A formula
+get_group <- function(x) {
+  out <- deparse(lhs(x))
+  out <- sub(".*\\(", "", out)
+  out <- sub(",.*", "", out)
+  return (out)
+}
+
+get_time <- function(x) {
+  out <- deparse(lhs(x))
+  out <- sub("\\).*", "",out)
+  out <- sub(".*, ", "", out)
+  return (out)
+}
+
+# Get left hand side of a formula
+# @param x A formula
+lhs <- function(x) {
+  return(terms(x)[[2]])
 }
