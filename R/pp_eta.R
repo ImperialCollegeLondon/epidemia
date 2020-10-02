51e0cd5659ab9d8bca4ed82041debf183d543d05
jscott6 <james.scott15@imperial.ac.uk>
2020-10-02 17:50:51 +0100

    Updated new_rw_stanmat

--- R/pp_eta.R
+++ R/pp_eta.R
@@ -70,6 +70,11 @@ pp_eta_ac <- function(object, stanmat) {
   stanmat_orig <- stanmat
   #return(list(stanmat_orig=stanmat_orig,stanmat=stanmat, z=z, object=object))
   stanmat <- new_rw_stanmat(object, stanmat)
+
+  print(paste0("stanmat: ", dim(stanmat))
+  print(paste0("stanmat: ", colnames(stanmat))
+  print(paste0("z: ", dim(z)))
+  print(paste0("z: ", colnames(z)))
   return(linear_predictor(stanmat, z))
 } 
 
@@ -88,6 +93,7 @@ new_rw_stanmat <- function(object, stanmat) {
   # )
 
   newnms <-  paste0(nme, "|", colnames(object$autocor$Z))
+  newnms <- grep("NA", newnms, invert=TRUE, value=TRUE)
 
   df <- parse_rw_labels(newnms)
   df$name <- newnms
