3308ff83bbc848ea301bc050cd3ff74d38f69817
jscott6 <james.scott15@imperial.ac.uk>
2021-01-06 15:28:48 +0000

    Adding europe covid vignette

--- R/as.matrix.epimodel.R
+++ R/as.matrix.epimodel.R
@@ -16,8 +16,13 @@
 #' @param regex_pars Character vector of regular expressions against which to match parameter names.Same as \link[rstanarm]{as.matrix.stanreg}
 #' @return A \code{matrix}, \code{array} or \code{data.frame} object.
 #' @export
-as.matrix.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL) {
+as.matrix.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL, par_models = NULL,
+                        par_types = NULL, par_groups = NULL) {
+
   pars <- collect_pars(x, pars, regex_pars)
+  pars <- restrict_pars(x, pars = pars, par_models = par_models,
+                        par_types = par_types, par_groups = par_groups)
+
   user_pars <- !is.null(pars)
   
   mat <- as.matrix(x$stanfit)
@@ -35,8 +40,13 @@ as.matrix.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL) {
 
 #' @rdname as.matrix.epimodel
 #' @export
-as.array.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL) {
+as.array.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL, par_models = NULL,
+                        par_types = NULL, par_groups = NULL) {
+
   pars <- collect_pars(x, pars, regex_pars)
+  pars <- restrict_pars(x, pars = pars, par_models = par_models,
+                        par_types = par_types, par_groups = par_groups)
+
   arr <- as.array(x$stanfit)
   if (identical(arr, numeric(0)))
     STOP_no_draws()
@@ -57,7 +67,8 @@ as.array.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL) {
 #' @rdname as.matrix.epimodel
 #' @method as.data.frame epimodel
 #' @export
-as.data.frame.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL) {
+as.data.frame.epimodel <- function(x, ..., pars = NULL, regex_pars = NULL, par_models = NULL,
+                        par_types = NULL, par_groups = NULL) {
   mat <- as.matrix.epimodel(x, pars = pars, regex_pars = regex_pars, ...)
   as.data.frame(mat)
 }
